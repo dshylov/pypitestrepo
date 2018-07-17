2c351693ca81526a6484b70f6ad7d8166935ee39
dshylov <creedqq@gmail.com>
2018-07-17 14:52:14 +0300

    init commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='maprdb_python_client',
+      version='1.0.18070301',
+      description='MapR-DB Python Client',
+      url='https://github.com/mapr/maprdb-python-client/',
+      author='MapR, Inc.',
+      keywords='ojai python client mapr maprdb',
+      packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
+      install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.0',
+                        'python-dateutil>=2.6.1', 'retrying>=1.3.3'],
+      python_requires='==2.7.*',
+      long_description=long_description
+      )
